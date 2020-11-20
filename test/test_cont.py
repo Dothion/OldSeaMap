a367d82da65ae6758d9c5e0917f2e5b664360aec
Dothion <Dothion@outlook.com>
2020-11-20 15:31:05 +0800

    update cont.py

--- test/test_cont.py
+++ test/test_cont.py
@@ -10,23 +10,27 @@ class ContTestCase(unittest.TestCase):
         self.assertEqual(Cont.of(5).bind(lambda x: Cont.of(x + 2)).run(identity), 7)
 
     def test_call_cc(self):
-        def cond(k, a, b):
+        def cond(k, a, b):  # No short circuit eval. Only used for demonstration.
             return a if k else b
 
         func = (lambda x: Cont.call_cc(
             lambda exit1: (
-                cond(x == 'e1', exit1('exited from exit1'), Cont.call_cc(
+                cond(x == 1, exit1((x + 1, 'exit1')), Cont.call_cc(
                     lambda exit2: (
-                        cond(x == 'e2', exit2('exited from exit2'), Cont.of('exited from end'))))))))
+                        cond(x == 2, exit2((x * 2, 'exit2')), Cont.of((x - 3, 'end')))))))))
+        safe_div = (lambda y: lambda x: Cont.call_cc(
+            lambda err: (err('Error: Divide by zero.') if y == 0 else Cont.of(x / y))))
         self.assertEqual(Cont.of(5)
                          .bind(lambda x: Cont.call_cc(lambda exit1: exit1(-1) if x == 5 else Cont.of(5)))
                          .run(identity), -1)
         self.assertEqual(Cont.of(4)
                          .bind(lambda x: Cont.call_cc(lambda exit1: exit1(-1) if x == 5 else Cont.of(5)))
                          .run(identity), 5)
-        self.assertEqual(Cont.of('e1').bind(func).run(identity), 'exited from exit1')
-        self.assertEqual(Cont.of('e2').bind(func).run(identity), 'exited from exit2')
-        self.assertEqual(Cont.of('e0').bind(func).run(identity), 'exited from end')
+        self.assertEqual(Cont.of(1).bind(func).run(identity), (2, 'exit1'))
+        self.assertEqual(Cont.of(2).bind(func).run(identity), (4, 'exit2'))
+        self.assertEqual(Cont.of(3).bind(func).run(identity), (0, 'end'))
+        self.assertEqual(Cont.of(5).bind(safe_div(5)).run(identity), 1)
+        self.assertEqual(Cont.of(5).bind(safe_div(0)).run(identity), 'Error: Divide by zero.')
 
     def test_functor(self):
         self.assertEqual(Cont.of(5).map(lambda x: x / 5), Cont.of(1))
