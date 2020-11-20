99da2e03d67fcd8e93aef1146a6bb6913259bc83
Dothion <Dothion@outlook.com>
2020-11-20 18:04:49 +0800

    fix cont.py

--- OldSeaMap/cont.py
+++ OldSeaMap/cont.py
@@ -16,28 +16,28 @@ from .typeclasses.monad import Monad
 _SometimesCallable = Union[Callable, _a]
 
 
-def call_cc(func: Callable[[Callable[[_a], Cont[_r, _b]]], Cont[_r, _a]]) -> Cont[_r, _a]:
-    return Cont(lambda c: curry(func)(lambda a: Cont(lambda _: c(a)).run(identity)).run(c))
+def call_cc(fn: Callable[[Callable[[_a], Cont[_r, _b]]], Cont[_r, _a]]) -> Cont[_r, _a]:
+    return Cont(lambda c: fn(lambda a: Cont(lambda _: c(a))).run(c))
 
 
 class Cont(Monad[_a]):
     def __init__(self: Cont[_r, _a], something: _a):
-        self._composing = lambda k: k(something if not isinstance(something, Callable) else curry(something))
+        self._composing = curry(something) if isinstance(something, Callable) else something
 
     def run(self: Cont[_r, _a], k: Callable[[_a], _r]) -> _r:
-        return self._composing(curry(k))
+        return self._composing(k)
 
     def bind(self: Cont[_r, _a], func: Callable[[_a], Cont[_r, _b]]) -> Cont[_r, _b]:
-        return Cont(lambda k: self.run(lambda a: func(a).run(k))).run(identity)
+        return Cont(lambda k: self.run(lambda a: func(a).run(k)))
 
     @classmethod
-    def of(cls, something: _SometimesCallable[_a]) -> Cont[_r, _SometimesCallable[_a]]:
-        return Cont(something)
-
-    def __eq__(self: Cont[_r, _a], other: Cont[_r, _b]) -> bool:
-        return self.run(identity) == other.run(identity)
+    def of(cls, something: _SometimesCallable[_a]) -> Cont[_SometimesCallable[_a]]:
+        return Cont(curry(lambda k: k(something)))
 
     def __repr__(self):
         return f'<Cont {self.run(identity)}>'
+
+    def __eq__(self, other: Cont):
+        return self.run(identity) == other.run(identity)
 
     call_cc = staticmethod(call_cc)
